399be1c856fcd578543f67a2a77c8b6de39a811f
Pranav P S <pranavps2theuniverse@gmail.com>
2025-06-19 23:20:15 +0530

    Two Pointers completed

--- TwoPointers/LongestMountainInArray.java
+++ TwoPointers/LongestMountainInArray.java
@@ -1,6 +1,34 @@
-// LeetCode Problem: LongestMountainInArray
+// LeetCode Problem: LongestMountainInArray 845
 // Solution by: @Pranav-P-S
 
-public class LongestMountainInArray {
-    // Your solution here
+class Solution {
+    public int longestMountain(int[] arr) {
+        int n = arr.length;
+        int result = 0;
+        int i = 1;
+        
+        while (i < n - 1) {
+            // Check if current is a peak
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                int left = i - 1;
+                int right = i + 1;
+                
+                // Expand left
+                while (left > 0 && arr[left] > arr[left - 1]) {
+                    left--;
+                }
+                
+                // Expand right
+                while (right < n - 1 && arr[right] > arr[right + 1]) {
+                    right++;
+                }
+                
+                result = Math.max(result, right - left + 1);
+                i = right;  // Skip to end of current mountain
+            } else {
+                i++;
+            }
+        }
+        return result;
+    }
 }
--- TwoPointers/SquaresOfASortedArray.java
+++ TwoPointers/SquaresOfASortedArray.java
@@ -1,6 +1,22 @@
-// LeetCode Problem: SquaresOfASortedArray
+// LeetCode Problem: SquaresOfASortedArray 977
 // Solution by: @Pranav-P-S
 
-public class SquaresOfASortedArray {
-    // Your solution here
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
 }
