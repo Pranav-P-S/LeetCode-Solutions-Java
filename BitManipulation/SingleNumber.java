05840c228f76ad1612aa5fff5563dea9e57aa6d5
Pranav P S <pranavps2theuniverse@gmail.com>
2025-06-20 23:52:32 +0530

    Bit Manipulation completed

--- BitManipulation/SingleNumber.java
+++ BitManipulation/SingleNumber.java
@@ -1,6 +1,12 @@
-// LeetCode Problem: SingleNumber
+// LeetCode Problem: SingleNumber 136
 // Solution by: @Pranav-P-S
 
-public class SingleNumber {
-    // Your solution here
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor^=nums[i];
+        }
+        return xor;
+    }
 }
