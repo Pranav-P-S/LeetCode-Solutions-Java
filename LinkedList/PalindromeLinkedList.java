78979bf12784f3848587ff54c46a1cc21dfe2a7f
Pranav P S <pranavps2theuniverse@gmail.com>
2025-06-22 15:25:37 +0530

    Linked Lists completed

--- LinkedList/PalindromeLinkedList.java
+++ LinkedList/PalindromeLinkedList.java
@@ -1,6 +1,54 @@
-// LeetCode Problem: PalindromeLinkedList
+// LeetCode Problem: PalindromeLinkedList 234
 // Solution by: @Pranav-P-S
 
-public class PalindromeLinkedList {
-    // Your solution here
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        
+        // Step 1: Find the middle of the list
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // Step 2: Reverse the second half
+        ListNode prev = null;
+        ListNode curr = slow;
+        
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        // Step 3: Compare both halves
+        ListNode first = head;
+        ListNode second = prev;
+        
+        while (second != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        
+        return true;
+    }
 }
--- LinkedList/ReverseLinkedListII.java
+++ LinkedList/ReverseLinkedListII.java
@@ -1,6 +1,42 @@
-// LeetCode Problem: ReverseLinkedListII
+// LeetCode Problem: ReverseLinkedListII 92
 // Solution by: @Pranav-P-S
 
-public class ReverseLinkedListII {
-    // Your solution here
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || head.next == null || left == right) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        // Move prev to the node before the left position
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode start = prev.next;
+        ListNode then = start.next;
+
+        // Reverse the sublist between left and right
+        for (int i = 0; i < right - left; i++) {
+            start.next = then.next;
+            then.next = prev.next;
+            prev.next = then;
+            then = start.next;
+        }
+
+        return dummy.next;
+    }
 }
