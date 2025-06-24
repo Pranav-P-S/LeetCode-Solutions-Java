07bcf8d66decbf0c684eba86f726d67f9ce6944d
Pranav P S <pranavps2theuniverse@gmail.com>
2025-06-24 22:02:19 +0530

    3 trees qns

--- BinaryTrees/MaximumDepthOfBinaryTree.java
+++ BinaryTrees/MaximumDepthOfBinaryTree.java
@@ -1,6 +1,41 @@
-// LeetCode Problem: MaximumDepthOfBinaryTree
+// LeetCode Problem: MaximumDepthOfBinaryTree 104
 // Solution by: @Pranav-P-S
 
-public class MaximumDepthOfBinaryTree {
-    // Your solution here
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode root, int depth) {
+        if (root == null) {
+            return 0; // Return "infinity" for null nodes
+        }
+        
+        // If it's a leaf node, return current depth + 1
+        if (root.left == null && root.right == null) {
+            return depth + 1;
+        }
+        
+        // Recursively find min depth of left and right subtrees
+        int leftDepth = dfs(root.left, depth + 1);
+        int rightDepth = dfs(root.right, depth + 1);
+        
+        return Math.max(leftDepth, rightDepth);
+    }
 }
--- BinaryTrees/MinimumDepthOfBinaryTree.java
+++ BinaryTrees/MinimumDepthOfBinaryTree.java
@@ -1,6 +1,41 @@
-// LeetCode Problem: MinimumDepthOfBinaryTree
+// LeetCode Problem: MinimumDepthOfBinaryTree 111
 // Solution by: @Pranav-P-S
 
-public class MinimumDepthOfBinaryTree {
-    // Your solution here
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null) return 0;
+        return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode root, int depth) {
+        if (root == null) {
+            return Integer.MAX_VALUE; // Return "infinity" for null nodes
+        }
+        
+        // If it's a leaf node, return current depth + 1
+        if (root.left == null && root.right == null) {
+            return depth + 1;
+        }
+        
+        // Recursively find min depth of left and right subtrees
+        int leftDepth = dfs(root.left, depth + 1);
+        int rightDepth = dfs(root.right, depth + 1);
+        
+        return Math.min(leftDepth, rightDepth);
+    }
 }
