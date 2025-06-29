555ec2435f6084cbe9da6a5b8b6442b23180655b
Pranav P S <pranavps2theuniverse@gmail.com>
2025-06-29 20:32:26 +0530

    tickets

--- Queues/TimeNeededToBuyTickets.java
+++ Queues/TimeNeededToBuyTickets.java
@@ -1,6 +1,21 @@
-// LeetCode Problem: TimeNeededToBuyTickets
+// LeetCode Problem: TimeNeededToBuyTickets 2073
 // Solution by: @Pranav-P-S
 
-public class TimeNeededToBuyTickets {
-    // Your solution here
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int count=0;
+        int point=0;
+        int length=tickets.length;
+        while(tickets[k]>0){
+            point=point%length;
+            while(tickets[point]==0){
+                point++;
+                point=point%length;
+            }
+            tickets[point]--;
+            count++;
+            point++;
+        }
+        return count;
+    }
 }
